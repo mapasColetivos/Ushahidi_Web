9b934b33faa5cca696bc69f4d8877f63d2f2a276
Emmanuel Kala <emkala@gmail.com>
2012-09-26 21:47:54 +0300

    Delete unnecessary view files

--- application/views/create_layer.php
+++
@@ -1,4 +0,0 @@
-<div class="layer_description" id="layer<?php echo $layer_id?>" data-id="<?php echo $layer_id?>" >
-	<span class="layer_color" style="background:#<?php echo $layer_color ?>">&nbsp;&nbsp;&nbsp;&nbsp;</span>
-	<a class="select_layer" ><?php echo $layer_name; ?></a> <a class="edit_layer" rel="layer" href='<?php echo url::base() ?>index.php/layers/edit/<?php echo $layer_id?>'>(editar)</a> <a class="delete_layer">(X)</a></br> 
-</div>
